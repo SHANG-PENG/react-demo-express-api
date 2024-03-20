cba2ed86724679d8cf4c3d9b8a243da76a871b16
SHANG-PENG <peng.shang.gm@gmail.com>
2024-03-20 14:48:17 +0800

    Update epay.js

--- router/epay.js
+++ router/epay.js
@@ -11,32 +11,34 @@ var getTransactionAmount = require('../db/amount').getTransactionAmount;
 // ToDo Payment Form 加载失败的问题
 
 router.get('/load-form', (req, res) => {
-    // const { gid, id } = req.query;
+    const { gid, id } = req.query;
     // 1). 根据这个 gid 获取基本信息 & 做校验
 
     // const referer = req.headers.referer;
-
+    
     try {
-        // var  submitBaseUrl = 'http://localhost:8080'
-        var submitBaseUrl = 'https://react-demo-express-api.vercel.app'
-
+        var  submitBaseUrl = 'http://localhost:8080'
+        // var submitBaseUrl= 'https://react-demo-express-api.vercel.app'
+        // // if (referer && referer.indexOf('localhost') === -1) {
+        // if (referer !== 'http://localhost:8080') {
+        //     submitBaseUrl = 'https://react-demo-express-api.vercel.app'
+        // }
         // 2). 返回支付表单脚本
-        // const scriptPath = path.join(__dirname, '../assets/js/payment-form.js');
-        // console.log('500 - scriptPath: ', scriptPath)
-        console.log('500 - ok')
-        // fs.readFile(scriptPath, 'utf8', (err, data) => {
-        //     if (err) {
-        //         console.error(`Error reading script file: ${err}`);
-        //         return res.status(500).send(`Internal Server Error: ${err}, ${scriptPath}`);
-        //     }
-        //     res.set('Content-Type', 'application/javascript');
+        const scriptPath = path.join(__dirname, '../assets/js/payment-form.js');
 
-        //     // 根据 id 从数据库或者通过获取金额
-        //     getTransactionAmount(id, (err, amount) => {
-        //         var nData = data.replace(/\$\$Amount\$\$/g, `$${amount}.00`).replace(/\$\$SubmitUrl\$\$/g, `${submitBaseUrl}/api/epay/submit-form`)
-        //         res.send(nData); // 发送脚本内容  
-        //     });
-        // });
+        fs.readFile(scriptPath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(`Error reading script file: ${err}`);
+                return res.status(500).send(`Internal Server Error: ${err}, ${scriptPath}`);
+            }
+            res.set('Content-Type', 'application/javascript');
+
+            // 根据 id 从数据库或者通过获取金额
+            getTransactionAmount(id, (err, amount) => {
+                var nData = data.replace(/\$\$Amount\$\$/g, `$${amount}.00`).replace(/\$\$SubmitUrl\$\$/g, `${submitBaseUrl}/api/epay/submit-form`)
+                res.send(nData); // 发送脚本内容  
+            });
+        });
     } catch (error) {
         console.log('error', error)
         res.send(error)
