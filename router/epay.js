8210a61482df68b8ca59ecd573df4a25edeb014c
SHANG-PENG <peng.shang.gm@gmail.com>
2024-03-20 13:51:54 +0800

    test

--- router/epay.js
+++ router/epay.js
@@ -13,32 +13,29 @@ var getTransactionAmount = require('../db/amount').getTransactionAmount;
 router.get('/load-form', (req, res) => {
     const { gid, id } = req.query;
     // 1). 根据这个 gid 获取基本信息 & 做校验
-    const referer = req.headers.referer;
-    
+
+    // const referer = req.headers.referer;
+
     try {
-        var  submitBaseUrl = 'http://localhost:8080'
-        // if (referer && referer.indexOf('localhost') === -1) {
-        if (referer !== 'http://localhost:8080') {
-            submitBaseUrl = 'https://react-demo-express-api.vercel.app'
-        }
+        // var  submitBaseUrl = 'http://localhost:8080'
+        var submitBaseUrl = 'https://react-demo-express-api.vercel.app'
+
         // 2). 返回支付表单脚本
         const scriptPath = path.join(__dirname, '../assets/js/payment-form.js');
 
-        // fs.readFile(scriptPath, 'utf8', (err, data) => {
-        //     if (err) {
-        //         console.error(`Error reading script file: ${err}`);
-        //         return res.status(500).send(`Internal Server Error: ${err}, ${scriptPath}`);
-        //     }
-        //     res.set('Content-Type', 'application/javascript');
+        fs.readFile(scriptPath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(`Error reading script file: ${err}`);
+                return res.status(500).send(`Internal Server Error: ${err}, ${scriptPath}`);
+            }
+            res.set('Content-Type', 'application/javascript');
 
-        //     // 根据 id 从数据库或者通过获取金额
-        //     getTransactionAmount(id, (err, amount) => {
-        //         var nData = data.replace(/\$\$Amount\$\$/g, `$${amount}.00`).replace(/\$\$SubmitUrl\$\$/g, `${submitBaseUrl}/api/epay/submit-form`)
-        //         res.send(nData); // 发送脚本内容  
-        //     });
-        // });
-
-        res.send("ok")
+            // 根据 id 从数据库或者通过获取金额
+            getTransactionAmount(id, (err, amount) => {
+                var nData = data.replace(/\$\$Amount\$\$/g, `$${amount}.00`).replace(/\$\$SubmitUrl\$\$/g, `${submitBaseUrl}/api/epay/submit-form`)
+                res.send(nData); // 发送脚本内容  
+            });
+        });
     } catch (error) {
         console.log('error', error)
         res.send(error)
