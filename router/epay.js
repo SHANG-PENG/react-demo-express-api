75a93e083892403df33b149f85277e5fc4fbe4cf
SHANG-PENG <peng.shang.gm@gmail.com>
2024-03-20 10:43:41 +0800

    test: debug load epay form

--- router/epay.js
+++ router/epay.js
@@ -16,25 +16,30 @@ router.get('/load-form', (req, res) => {
     const referer = req.headers.referer
     console.log('referer', referer)
     const submitBaseUrl = 'http://localhost:8080'
-    if (referer?.indexOf('localhost') === -1) {
-        submitBaseUrl = 'https://react-demo-express-api.vercel.app'
-    }
-    // 2). 返回支付表单脚本
-    const scriptPath = path.join(__dirname, '../assets/js/payment-form.js');
-
-    fs.readFile(scriptPath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error reading script file: ${err}`);
-            return res.status(500).send('Internal Server Error');
+    try {
+        if (referer && referer.indexOf('localhost') === -1) {
+            submitBaseUrl = 'https://react-demo-express-api.vercel.app'
         }
-        res.set('Content-Type', 'application/javascript');
+        // 2). 返回支付表单脚本
+        const scriptPath = path.join(__dirname, '../assets/js/payment-form.js');
 
-        // 根据 id 从数据库或者通过获取金额
-        getTransactionAmount(id, (err, amount) => {
-            var nData = data.replace(/\$\$Amount\$\$/g, `$${amount}.00`).replace(/\$\$SubmitUrl\$\$/g, `${submitBaseUrl}/api/epay/submit-form`)
-            res.send(nData); // 发送脚本内容  
+        fs.readFile(scriptPath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(`Error reading script file: ${err}`);
+                return res.status(500).send('Internal Server Error');
+            }
+            res.set('Content-Type', 'application/javascript');
+
+            // 根据 id 从数据库或者通过获取金额
+            getTransactionAmount(id, (err, amount) => {
+                var nData = data.replace(/\$\$Amount\$\$/g, `$${amount}.00`).replace(/\$\$SubmitUrl\$\$/g, `${submitBaseUrl}/api/epay/submit-form`)
+                res.send(nData); // 发送脚本内容  
+            });
         });
-    });
+    } catch (error) {
+        console.log('error', error)
+        res.send(error)
+    }
 });
 
 router.post('/submit-form', (req, res) => {
